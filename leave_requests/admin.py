7ead439d48120693af2904092b0e7a76335239ef
Venelin Yankov <venelin.yankov@gmail.com>
2026-04-17 00:17:04 +0300

    Register LeaveRequest to admin panel

--- leave_requests/admin.py
+++ leave_requests/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from leave_requests.models import LeaveRequest
 
-# Register your models here.
+
+@admin.register(LeaveRequest)
+class LeaveRequestAdmin(admin.ModelAdmin):
+
+    list_display = ['employee', 'leave_type', 'duration_off', 'start_date', 'end_date', 'created_at']
+
+    list_filter = ['leave_type', 'start_date']
+
+
