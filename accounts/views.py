261ce1a9f49c70c910779cc6e36648994f6fa7da
Venelin Yankov <venelin.yankov@gmail.com>
2026-05-14 23:24:45 +0300

    Adjust Home page and finalize dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,8 @@ from django.views.generic import CreateView, DetailView
 
 from accounts.forms import EmployeeRegisterForm
 from accounts.models import EmployeeUser
+from balances.models import LeaveBalance
+from leave_requests.models import LeaveRequest
 
 
 class EmployeeRegister(CreateView):
@@ -28,3 +30,12 @@ class EmployeeDetailView(LoginRequiredMixin, DetailView):
 
     def get_object(self, queryset = None):
         return self.request.user
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['balance'] = LeaveBalance.objects.filter(employee=self.request.user).first()
+        context['recent_requests'] = LeaveRequest.objects.filter(
+            employee=self.request.user
+        ).order_by('-id')[:5]
+        return context
